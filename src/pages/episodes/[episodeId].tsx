52b2c12ac26000002bcd55c69adfab84eef15ea5
Gabriel Masson <g.masson010@gmail.com>
2021-04-22 20:03:40 -0300

    feat: Adds episode page

--- src/pages/episodes/[episodeId].tsx
+++ src/pages/episodes/[episodeId].tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-danger */
+import Link from 'next/link';
 import Image from 'next/image';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { ptBR } from 'date-fns/locale';
@@ -61,23 +63,40 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
 const Episode: NextPage<EpisodeProps> = ({ episode }) => {
   return (
-    <div className={styles.episode}>
-      <div className={styles.thubmailContainer}>
-        <button type="button">
-          <img src="/arrow-left.svg" alt="Voltar" />
-        </button>
+    <div className={styles.container}>
+      <div className={styles.thumbnailContainer}>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar" />
+          </button>
+        </Link>
 
         <Image
           width={700}
-          height={160}
+          height={220}
           src={episode.thumbnail}
           alt={episode.title}
         />
 
-<button type="button">
-          <img src="/arrow-left.svg" alt="Voltar" />
+        <button type="button">
+          <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+
+        <span>{episode.members}</span>
+
+        <span>{episode.publishedAt}</span>
+
+        <span>{episode.durationAsString}</span>
+      </header>
+
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: episode.description }}
+      />
     </div>
   );
 };
